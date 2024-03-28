1bdcd9dc92539493944309da0d7b632a086c5a6a
EMCT-Daniel <daniel.h@emct.tw>
2024-03-28 11:11:55 +0800

    Chapter 1 - Extracting Frequent Renter Points
    - Extract Method
    - Move Method

--- src/main/java/tw/dh46/example/videostore/Rental.java
+++ src/main/java/tw/dh46/example/videostore/Rental.java
@@ -37,4 +37,15 @@ class Rental {
         }
         return result;
     }
+
+    int getFrequentRenterPoints() {
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                && getDaysRented() > 1) {
+            return 2;
+        } else {
+            // add frequent renter points
+            return 1;
+        }
+    }
 }
