4f20b2f95257a4a70752cce2cb2dc30932ff4bc2
EMCT-Daniel <daniel.h@emct.tw>
2024-03-28 14:06:25 +0800

    Chapter 1 - Replacing the Conditional Logic on Price Code with Polymorphism: Move Method

--- src/main/java/tw/dh46/example/videostore/Rental.java
+++ src/main/java/tw/dh46/example/videostore/Rental.java
@@ -1,7 +1,7 @@
 package tw.dh46.example.videostore;
 
-class Rental {
-    private Movie _movie;
+public class Rental {
+    Movie _movie;
     private int _daysRented;
 
     public Rental(Movie movie, int daysRented) {
@@ -17,35 +17,11 @@ class Rental {
         return _movie;
     }
 
-    public double getCharge() {
-        double result = 0;
-        // determine amounts for aRental line
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+    int getFrequentRenterPoints() {
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 
-    int getFrequentRenterPoints() {
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                && getDaysRented() > 1) {
-            return 2;
-        } else {
-            // add frequent renter points
-            return 1;
-        }
+    public double getCharge() {
+        return _movie.getCharge(_daysRented);
     }
 }
